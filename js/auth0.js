214f6139041f6d9ecf4917f9472ec1ae8537032e
Mike Solomon <msolomon@thrillist.com>
2016-06-11 18:10:22 -0400

    guest book is working

---
+++ js/auth0.js
@@ -0,0 +1,33 @@
+var lock = new Auth0Lock('oW84zchhtMj6g53ZZDhfKKoSj3Jt9C16', 'beaachhouseboiz.auth0.com');
+
+document.getElementById('btn-login').addEventListener('click', function() {
+  lock.show({
+    authParams: { scope: 'openid' },
+    icon: 'https://raw.githubusercontent.com/msolomonTMG/obpbeach.house/gh-pages/images/beachball.png'
+  });
+});
+
+var hash = lock.parseHash(window.location.hash);
+if (hash) {
+  if (hash.error) {
+    console.log("There was an error logging in", hash.error);
+    alert('There was an error: ' + hash.error + '\n' + hash.error_description);
+  } else {
+    //save the token in the session:
+    localStorage.setItem('id_token', hash.id_token);
+  }
+}
+
+//retrieve the profile:
+var id_token = localStorage.getItem('id_token');
+if (id_token) {
+  lock.getProfile(id_token, function (err, profile) {
+    if (err) {
+      return alert('There was an error getting the profile: ' + err.message);
+    }
+    $('#login-button-section').hide();
+    $('#sign-guest-book-button-section').show();
+    $('.form-group').prop("disabled", false);
+    document.getElementById('name').textContent = profile.name;
+  });
+}
